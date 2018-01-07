33e44e8ef23106bb3a5e5c9a4843758b81e1d009
vojta001 <vojtech.kane@gmail.com>
2018-01-07 16:09:41 +0100

    Better UX

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from __future__ import print_function
 from cmd import Cmd
 import getpass
 import six
+import os
 
 try:
 	from bakalari.bakalari import BakalariAPI
 except ImportError:
 	import sys
 	try:
-		import os
 		sys.path.insert(0, os.path.abspath("bakalari"))
 		from bakalari.bakalari import BakalariAPI
 	except ImportError:
@@ -18,6 +18,7 @@ except ImportError:
 class MainPrompt(Cmd):
 	username = ""
 	password = ""
+	loggedIn = False
 	url = ""
 	api = None
 	marks = None
@@ -40,6 +41,9 @@ class MainPrompt(Cmd):
 			return float(0)
 
 	def get_api(self):
+		if not self.loggedIn:
+			print("You have to be logged in!")
+			self.onecmd("login")
 		if self.api == None:
 			self.api = BakalariAPI(self.username, self.password, self.url)
 			self.api.login()
@@ -47,14 +51,49 @@ class MainPrompt(Cmd):
 
 	def do_login(self, args):
 		"""Log in to be able to make API calls (which is necessary to view the marks)"""
+		eUrl = None
+		eUsr = None
+		ePas = None
+		if "bakalariUrl" in os.environ:
+			eUrl = os.environ["bakalariUrl"]
+		if "bakalariUsr" in os.environ:
+			eUsr = os.environ["bakalariUsr"]
+			if "bakalariPas" in os.environ:
+				ePas = os.environ["bakalariPas"]
 		try:
-			self.url = raw_input("url: ")
-			self.username = raw_input("username: ")
+			if eUrl == None:
+				self.url = raw_input("url: ")
+			else:
+				self.url = raw_input("url [%s]: " % (eUrl))
+				if self.url == "":
+					self.url = eUrl
+			if eUsr == None:
+				self.username = raw_input("username: ")
+			else:
+				self.username = raw_input("username [%s]: " % (eUsr))
+				if self.username == "":
+					self.username = eUsr
 		except NameError:
-			self.url = input("url: ")
-			self.username = input("username: ")
-		self.password = getpass.getpass("password: ")
+			if eUrl == None:
+				self.url = input("url: ")
+			else:
+				self.url = input("url [%s]: " % (eUrl))
+				if self.url == "":
+					self.url = eUrl
+			if eUsr == None:
+				self.username = input("username: ")
+			else:
+				self.username = input("username [%s]: " % (eUsr))
+				if self.username == "":
+					self.username = eUsr
+		if ePas == None:
+			self.password = getpass.getpass("password: ")
+		else:
+			self.password = getpass.getpass("password [<password>]: ")
+			if self.password == "":
+				self.password = ePas
 		self.api = None
+		self.loggedIn = True
 
 	def do_update(self, args):
 		"""Flush the cache and download new marks"""
@@ -63,6 +102,9 @@ class MainPrompt(Cmd):
 
 	def do_all(self, args):
 		"""Print summary of all marks"""
+		if self.marks == None:
+			print("Updating...")
+			self.onecmd("update")
 		for subject in self.marks:
 			print(subject["zkratka"].upper(), end=": ")
 			wAvgSum = 0
